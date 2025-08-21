236a89200d6e0a9acc3d8f14d6a2a1aa79ad7377
Yuliia Krasnozhon <ykrasnozhen@ukr.net>
2025-08-21 17:22:54 +0300

    fixed notes file

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -3,7 +3,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from 'next/navigation';
 import { getSingleNote } from "@/lib/api";
-import css from './NoteDetail.module.css';
+import css from './NoteDetails.module.css';
 
 const NoteDetailsClient = () => {
 	const { id } = useParams<{ id: string }>();
